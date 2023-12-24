7a50162b909697bc0f0ca79445dc555c58508bb9
Nabillera <vatcharadzenini@gmail.com>
2023-12-24 22:32:36 +0400

    homework 6

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { AddTodo } from './AddTodo';
+import './App.css';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <AddTodo/>
+      
+    </div>
+  );
+}
+
+export default App;
