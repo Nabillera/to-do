308c814d722071cfa9db02e6fb43b30111971e0a
Nabillera <vatcharadzenini@gmail.com>
2023-12-28 02:10:54 +0400

    modified colors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
-import { AddTodo } from './components/AddTodo';
-import './App.css';
-import { Todo } from './components/Todo';
+    import React, { useState } from "react";
+    import { AddTodo } from "./components/AddTodo";
+    import "./App.css";
+    import { Todo } from "./components/Todo";
 
-function App() {
+    function App() {
+    const [todos, setTodos] = useState([]);
+    const colors = ["#CCCCFF", "#9EA9ED", "#7575CF"];
+    const [colorIndex, setColorIndex] = useState(0);
 
-  const [todos, setTodos] = useState([])
+    const saveData = (todo) => {
+        const newTodo = { ...todo, color: colors[colorIndex] };
+        setTodos([...todos, newTodo]);
+        setColorIndex((colorIndex + 1) % colors.length);
+    };
 
-  const saveData = (todo) => {
-    setTodos([...todos, todo])
-  }
-
-  return (
-    <div className="App">
-      <AddTodo onSave={saveData}/>
-      <div id="todos-container">
-      {todos.map((todo, index) => (
-        <Todo
-          key={index}
-          title={todo.title}
-          description={todo.description}
-          />
+    return (
+        <div className="App">
+        <AddTodo onSave={saveData} />
+        <div id="todos-container">
+        {todos.map((todo, index) => (
+            <Todo
+            key={index}
+            title={todo.title}
+            description={todo.description}
+            color={todo.color}
+            />
         ))}
-      </div>
-    </div>
-  );
-}
+        </div>
+        </div>
+    );
+    }
 
-export default App;
+    export default App;
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,17 +1,7 @@
-import { useEffect } from 'react'
 import '../styles/Todo.css'
 export function Todo(props){
-
-    // const colors = ["#CCCCFF", "#9EA9ED", "#7575CF"]
-    // const box = document.getElementById("todo-container")
-    // const setColor = () => {
-    //     box.style.backgroundColor = colors[props.key%3]
-    // }
-    // useEffect(() => {
-    //     setColor()   
-    // })
     
-    return (<div id="todo-container">
+    return (<div id="todo-container" style={{backgroundColor: props.color}}>
         <div id="todo-text-container">
             <p id="title">{props.title}</p>
             <p id="description">{props.description}</p>
